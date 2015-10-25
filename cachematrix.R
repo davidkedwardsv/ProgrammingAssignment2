33d44db4c7bafdfa63cd8d81274bc074c1a78657
David Edwards <davidkedwardsv@users.noreply.github.com>
2015-10-25 15:00:38 -0700

    completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# This assignment includes the following functions:
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Creates a special "matrix" object that can cache its inverse.
+    # Input "x" is a square matrix (assumed to be invertible).
+    
+    invm <- NULL
+    
+    #Sets the value of the vector
+    set = function(y) {
+        x <<- y
+        invm <<- NULL
+    }
+    
+    #Gets the value of the vector
+    get = function() x
+    
+    #Sets the value of the inverse
+    setinvm = function(inverse) invm <<- inverse
+    
+    #Gets the value of the inverse
+    getinvm = function() invm
+    list(set = set,
+    get = get,
+    setinvm = setinvm,
+    getinvm = getinvm)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Creates inverse of matrix evaluated by makeCacheMatrix().
+    # Input "x" is output of makeCacheMatrix()
+    
+    invm = x$getinvm()
+    
+    #Evaluates if mean has already been calculated
+    if (!is.null(invm)){
+        
+        #If yes, retrieve inverse from cache
+        message("getting cached data")
+        return(invm)
+    }
+    
+    #If no, calculate the inverse
+    invm.data = x$get()
+    invm = solve(invm.data, ...)
+    
+    #Sets the value of the inverse in the cache via "setinvm" function
+    x$setinvm(invm)
+    
+    return(invm)
 }
